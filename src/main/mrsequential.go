1d051d8f0a7ec617eaea42b7294d65b191cf3c41
hwoss.wu <hwoss.wu@anker-in.com>
2025-05-08 18:48:44 +0800

    结构体定义

--- src/main/mrsequential.go
+++ src/main/mrsequential.go
@@ -1,49 +1,52 @@
 package main
 
 //
-// simple sequential MapReduce.
+// 简单的顺序执行版 MapReduce
 //
-// go run mrsequential.go wc.so pg*.txt
+// 运行命令: go run mrsequential.go wc.so pg*.txt
 //
 
-import "fmt"
-import "6.5840/mr"
-import "plugin"
-import "os"
-import "log"
-import "io/ioutil"
-import "sort"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"plugin"
+	"sort"
 
-// for sorting by key.
+	"6.5840/mr"
+)
+
+// 用于按键排序
 type ByKey []mr.KeyValue
 
-// for sorting by key.
+// 用于按键排序的接口实现
 func (a ByKey) Len() int           { return len(a) }
 func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: mrsequential xxx.so inputfiles...\n")
+		fmt.Fprintf(os.Stderr, "用法: mrsequential xxx.so 输入文件...\n")
 		os.Exit(1)
 	}
 
 	mapf, reducef := loadPlugin(os.Args[1])
 
 	//
-	// read each input file,
-	// pass it to Map,
-	// accumulate the intermediate Map output.
+	// 读取每个输入文件,
+	// 将其传递给 Map 函数,
+	// 收集 Map 的中间输出结果
 	//
 	intermediate := []mr.KeyValue{}
 	for _, filename := range os.Args[2:] {
 		file, err := os.Open(filename)
 		if err != nil {
-			log.Fatalf("cannot open %v", filename)
+			log.Fatalf("无法打开文件 %v", filename)
 		}
 		content, err := ioutil.ReadAll(file)
 		if err != nil {
-			log.Fatalf("cannot read %v", filename)
+			log.Fatalf("无法读取文件 %v", filename)
 		}
 		file.Close()
 		kva := mapf(filename, string(content))
@@ -51,9 +54,9 @@ func main() {
 	}
 
 	//
-	// a big difference from real MapReduce is that all the
-	// intermediate data is in one place, intermediate[],
-	// rather than being partitioned into NxM buckets.
+	// 与真正的 MapReduce 的一个重大区别是所有
+	// 中间数据都存储在一个地方 intermediate[] 中，
+	// 而不是被分区到 NxM 个桶中
 	//
 
 	sort.Sort(ByKey(intermediate))
@@ -62,8 +65,8 @@ func main() {
 	ofile, _ := os.Create(oname)
 
 	//
-	// call Reduce on each distinct key in intermediate[],
-	// and print the result to mr-out-0.
+	// 对 intermediate[] 中的每个不同键调用 Reduce 函数，
+	// 并将结果打印到 mr-out-0 文件中
 	//
 	i := 0
 	for i < len(intermediate) {
@@ -77,7 +80,7 @@ func main() {
 		}
 		output := reducef(intermediate[i].Key, values)
 
-		// this is the correct format for each line of Reduce output.
+		// 这是 Reduce 输出的每一行的正确格式
 		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
 
 		i = j
@@ -86,21 +89,21 @@ func main() {
 	ofile.Close()
 }
 
-// load the application Map and Reduce functions
-// from a plugin file, e.g. ../mrapps/wc.so
+// 从插件文件加载应用程序的 Map 和 Reduce 函数
+// 例如从 ../mrapps/wc.so 文件加载
 func loadPlugin(filename string) (func(string, string) []mr.KeyValue, func(string, []string) string) {
 	p, err := plugin.Open(filename)
 	if err != nil {
-		log.Fatalf("cannot load plugin %v", filename)
+		log.Fatalf("无法加载插件 %v", filename)
 	}
 	xmapf, err := p.Lookup("Map")
 	if err != nil {
-		log.Fatalf("cannot find Map in %v", filename)
+		log.Fatalf("在 %v 中找不到 Map 函数", filename)
 	}
 	mapf := xmapf.(func(string, string) []mr.KeyValue)
 	xreducef, err := p.Lookup("Reduce")
 	if err != nil {
-		log.Fatalf("cannot find Reduce in %v", filename)
+		log.Fatalf("在 %v 中找不到 Reduce 函数", filename)
 	}
 	reducef := xreducef.(func(string, []string) string)
 
--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,64 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
 
 type Coordinator struct {
-	// Your definitions here.
+	TaskId     int        // 作为自增任务id
+	MapChan    chan *Task //map任务队列
+	ReduceChan chan *Task //reduce任务队列
+	ReduceNum  int        //reduce数量
+	files      []string   //传入文件数组
+	DistPhase  Phase      //目前框架中处于什么阶段
+	TaskMetaHolder  map[int]*TaskMetaInfo //主节点可以掌握所有任务，taskId对应
 
+	lock sync.RWMutex
 }
+type TaskMetaInfo struct {
+	state      TaskStatus   // 任务的状态
+	TaskAdr   *Task // 只需要存储对应的任务指针
+}
+type Phase int
 
-// Your code here -- RPC handlers for the worker to call.
+const (
+	MapPhase Phase = iota //此阶段在分发MapTask
+	ReducePhase
+	AllDone //任务分配完成
+)
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) GetTask(req *GetTaskReq, reply *GetTaskResp) error {
+	//我要给你map还是reduce任务呢？
+	currentPhase := c.DistPhase
+	if currentPhase == MapPhase {
+
+	}else if currentPhase == ReducePhase {
+
+	} else{
+
+	}
+
+	reply.Task = Task{
+		TaskId: c.generateId(),
+	TaskType: }
 	return nil
 }
 
+// 获取全局id的方法，这里用自增id
+func (c *Coordinator) generateId() int {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	res := c.TaskId
+	c.TaskId++
+	return res
+}
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +72,40 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
+// nReduce is the number of reduce tasks to use.文件多少其实也决定了多少个map
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
 
-	// Your code here.
+	c := Coordinator{
+		files:             files,
+		ReduceNum:        nReduce,
+		DistPhase:         MapPhase,
+		MapChan:    make(chan *Task, len(files)),
+		ReduceChan: make(chan *Task, nReduce),
+		lock: sync.RWMutex{},
+		TaskMetaHolder:make(map[int]*TaskMetaInfo, len(files)+nReduce), // 任务的总数应该是files + Reducer的数量
+		}
+	}
+	c.makeMapTasks(files)
 
+	c.server()
 
 	c.server()
 	return &c
+}
+
+func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
 }
